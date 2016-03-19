1daf37f6c45c8cc7eba399c0058af3f6e7807072
joshisen@me.com <isenhardt@yahoo.com>
2016-03-19 02:15:37 +0000

    Set up authentitication and navigation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'https://workspace-for-ruby-on-rails-yguyjosh.c9users.io', port:2587}
+  config.action_mailer.default_url_options = { host: '$IP', port: $PORT }
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
